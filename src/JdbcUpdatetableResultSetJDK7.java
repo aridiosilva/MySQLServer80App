7ddf8a166f5aa930151817fbf8b131e8b3e25a90
aridiosilva <aridiosilva@aridiosilva.com>
2020-12-19 10:50:44 -0200

    return to main repository and deleted merge in sources

--- src/JdbcUpdatetableResultSetJDK7.java
+++ src/JdbcUpdatetableResultSetJDK7.java
@@ -1,9 +1,10 @@
-<<<<<<< HEAD
 
 import java.sql.*;
  
-public class JdbcUpdatetableResultSetJDK7 {  // JDK 7 and above
+public class JdbcUpdatetableResultSetJDK7 {  
+	
    public static void main(String[] args) {
+	   
       try (
          Connection conn = DriverManager.getConnection(
                "jdbc:mysql://localhost:8888/ebookshop", "myuser", "xxxx"); // MySQL
@@ -60,67 +61,5 @@ public class JdbcUpdatetableResultSetJDK7 {  // JDK 7 and above
          ex.printStackTrace();
       }
    }
-=======
 
-import java.sql.*;
- 
-public class JdbcUpdatetableResultSetJDK7 {  // JDK 7 and above
-   public static void main(String[] args) {
-      try (
-         Connection conn = DriverManager.getConnection(
-               "jdbc:mysql://localhost:8888/ebookshop", "myuser", "xxxx"); // MySQL
- 
-         //  TYPE_SCROLL_INSENSITIVE: ResultSet is scrollable and does not
-         //    reflect changes by others while it is opened for processing.
-         //  CONCUR_UPDATABLE: Resultset is updatable
-         Statement stmt = conn.createStatement(
-               ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-      ) {
-         conn.setAutoCommit(false);  // Disable auto-commit
- 
-         ResultSet rset = stmt.executeQuery("select * from books");
- 
-         // Update a row
-         rset.last();
-         System.out.println("-- Update a row --");
-         System.out.println(
-                 rset.getRow() + ": " + + rset.getInt("id") + ", "
-                 + rset.getString("title") + ", " + rset.getString("author") + ", "
-                 + rset.getDouble("price") + ", " + rset.getInt("qty"));
-         rset.updateDouble("price", 99.99);   // update cells via column name
-         rset.updateInt("qty", 99);
-         rset.updateRow();  // update the row in the data source
-         System.out.println(
-                 rset.getRow() + ": " + + rset.getInt("id") + ", "
-                 + rset.getString("title") + ", " + rset.getString("author") + ", "
-                 + rset.getDouble("price") + ", " + rset.getInt("qty"));
- 
-         // Delete a row
-         rset.first();
-         System.out.println("-- Delete a row --");
-         System.out.println(
-                 rset.getRow() + ": " + + rset.getInt("id") + ", "
-                 + rset.getString("title") + ", " + rset.getString("author") + ", "
-                 + rset.getDouble("price") + ", " + rset.getInt("qty"));
-         rset.deleteRow();  // delete the current row
- 
-         // A updatable ResultSet has a special row that serves as a staging area
-         // for building a row to be inserted.
-         rset.moveToInsertRow();
-         rset.updateInt(1, 8001);  // Update cells via column number (starts at 1)
-         rset.updateString(2, "Even More Programming");
-         rset.updateString(3, "Kumar");
-         rset.updateDouble(4, 77.88);
-         rset.updateInt(5, 77);
-         rset.insertRow();
-         // After inserting a row, you need to move the cursor to another row, to avoid
-         //  unexpected error if another part of the program also accesses this ResultSet.
-         rset.beforeFirst();
- 
-         conn.commit();  // commit changes
-      } catch(SQLException ex) {
-         ex.printStackTrace();
-      }
-   }
->>>>>>> branch 'main' of https://github.com/aridiosilva/MySQLServer80App.git
 }
--- src/SupportedResultSetTypeTest.java
+++ src/SupportedResultSetTypeTest.java
@@ -1,9 +1,10 @@
-<<<<<<< HEAD
 
 import java.sql.*;
 
-public class SupportedResultSetTypeTest {  // JDK 7 and above
+public class SupportedResultSetTypeTest { 
+	
    public static void main(String[] args) {
+	   
       try (
          Connection conn = DriverManager.getConnection(
                "jdbc:mysql://localhost:8888/ebookshop", "myuser", "xxxx");  // MySQL
@@ -42,49 +43,5 @@ public class SupportedResultSetTypeTest {  // JDK 7 and above
          ex.printStackTrace();
       }
    }
-=======
 
-import java.sql.*;
-
-public class SupportedResultSetTypeTest {  // JDK 7 and above
-   public static void main(String[] args) {
-      try (
-         Connection conn = DriverManager.getConnection(
-               "jdbc:mysql://localhost:8888/ebookshop", "myuser", "xxxx");  // MySQL
-      ) {
-         DatabaseMetaData md = conn.getMetaData();   // Get the database metadata
-         // Verify ResultSet's type
-         System.out.println("-- ResultSet Type --");
-         System.out.println("Supports TYPE_FORWARD_ONLY: "
-                 + md.supportsResultSetType(ResultSet.TYPE_FORWARD_ONLY));
-         System.out.println("Supports TYPE_SCROLL_INSENSITIVE: "
-                 + md.supportsResultSetType(ResultSet.TYPE_SCROLL_INSENSITIVE));
-         System.out.println("Supports TYPE_SCROLL_SENSITIVE: "
-                 + md.supportsResultSetType(ResultSet.TYPE_SCROLL_SENSITIVE));
- 
-         // Verify ResultSet's concurrency
-         System.out.println("-- ResultSet Concurrency --");
-         System.out.println("Supports CONCUR_READ_ONLY: "
-                 + md.supportsResultSetType(ResultSet.CONCUR_READ_ONLY));
-         System.out.println("Supports CONCUR_UPDATABLE: "
-                 + md.supportsResultSetType(ResultSet.CONCUR_UPDATABLE));
- 
-         // Verify ResultSet's cursor holdability
-         System.out.println("-- ResultSet Cursor Holdability --");
-         int holdability = md.getResultSetHoldability();  // Get the default holdability
-         if (holdability == ResultSet.CLOSE_CURSORS_AT_COMMIT) {
-            System.out.println("Default: CLOSE_CURSORS_AT_COMMIT");
-         } else if (holdability == ResultSet.HOLD_CURSORS_OVER_COMMIT) {
-            System.out.println("Default: ResultSet.HOLD_CURSORS_OVER_COMMIT");
-         }
-         System.out.println("Supports CLOSE_CURSORS_AT_COMMIT: "
-                 + md.supportsResultSetType(ResultSet.CLOSE_CURSORS_AT_COMMIT));
-         System.out.println("Supports HOLD_CURSORS_OVER_COMMIT: "
-                 + md.supportsResultSetType(ResultSet.HOLD_CURSORS_OVER_COMMIT));
- 
-      } catch(SQLException ex) {
-         ex.printStackTrace();
-      }
-   }
->>>>>>> branch 'main' of https://github.com/aridiosilva/MySQLServer80App.git
 }
